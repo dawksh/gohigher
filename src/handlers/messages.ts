635396cdda2bcf5446536ef0e7f5ef6b05e696b8
daksh <akshd@outlook.com>
2024-12-25 19:44:23 +0530

    chore: add signup

--- src/handlers/messages.ts
+++ src/handlers/messages.ts
@@ -1,11 +1,12 @@
 import TelegramBot, { Message } from "node-telegram-bot-api";
+import { supabase } from "../lib/supabase";
 
 const handleMessage = async (msg: Message, bot: TelegramBot) => {
     const { chat, text } = msg;
 
     switch (text) {
         case "/start":
-            bot.sendMessage(chat.id, "gohigher.life");
+            startSession(chat.id, bot, msg.from?.username);
             break
         case "/join":
             bot.sendMessage(chat.id, "Join a new group");
@@ -16,8 +17,22 @@ const handleMessage = async (msg: Message, bot: TelegramBot) => {
     }
 }
 
-const startSession = () => {
-
+const startSession = async (chatId: number, bot: TelegramBot, username?: string) => {
+    const { data, error } = await supabase.from("users").select("*").eq("chat_id", chatId);
+    if (!error && data.length > 0) {
+        const user = data[0];
+        bot.sendMessage(chatId, `Welcome back ${user.username}!`)
+    } else {
+        const { data, error } = await supabase.from("users").upsert({
+            chat_id: chatId,
+            username
+        }).returns();
+        if (!error && data) {
+            bot.sendMessage(chatId, `Successfully registered`)
+            return
+        }
+        bot.sendMessage(chatId, `Something went wrong`)
+    }
 }
 
 export { handleMessage }
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,7 @@
 
 import { createClient } from "@supabase/supabase-js"
+import { config } from "dotenv"
+config()
 
 const supabase = createClient(process.env.SUPABASE_URL as string, process.env.SUPABASE_KEY as string)
 
