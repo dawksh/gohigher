d015f9f17467942251e14f45dd4d0f3ed8bbcd08
daksh <akshd@outlook.com>
2024-12-30 00:42:48 +0530

    feat: add messages

--- src/handlers/messages.ts
+++ src/handlers/messages.ts
@@ -6,10 +6,10 @@ const handleMessage = async (msg: Message, bot: TelegramBot) => {
 
     switch (text) {
         case "/start":
-            startSession(chat.id, bot, msg.from?.username);
+            await startSession(chat.id, bot, msg.from?.username);
             break
         case "/join":
-            bot.sendMessage(chat.id, "Join a new group");
+            await joinSession(chat.id, bot, text)
             break
         case "/verify":
             bot.sendMessage(chat.id, "Verify using Strava");
@@ -33,6 +33,64 @@ const startSession = async (chatId: number, bot: TelegramBot, username?: string)
         }
         bot.sendMessage(chatId, `Something went wrong`)
     }
+}
+
+const joinSession = async (chatId: number, bot: TelegramBot, text: string) => {
+    const { data: userData, error } = await supabase.from("users").select("*").eq("chat_id", chatId);
+
+    if (userData?.length == 0 || Boolean(error)) return bot.sendMessage(chatId, "Unexpected Error Occured")
+
+    const splitText = text.split(" ")
+    const TYPES = {
+        "10K Steps": "3d8d59b5-d46a-44af-9926-49fb914c433f"
+    }
+
+    if (splitText.length == 1) {
+
+        await bot.sendMessage(chatId, `
+Available Groups:
+${Object.keys(TYPES).map((el, idx) => `${idx + 1} -> ${el}`)}
+
+send \`/join <Group ID> <Max Users>\` to create a group.
+            `)
+        return
+    }
+    if (splitText.length == 2) {
+        const { data: groupData, error: groupError } = await supabase.from("groups").select("*").eq("id", splitText[1]);
+
+        if (!groupError && groupData.length > 0) {
+
+            const max = groupData[0].max_users;
+
+            if (max == groupData.length) {
+                await bot.sendMessage(chatId, "Group is already full");
+                return;
+            }
+
+            const { data, error } = await supabase.from("groups").insert({
+                id: splitText[1],
+                condition: splitText[1],
+                user: userData?.[0].id
+            })
+
+            if (error) {
+                bot.sendMessage(chatId, "Unexpected Error Occurred")
+                return
+            }
+
+        }
+    }
+
+    if (splitText.length == 3) {
+        const { data: groupData, error: groupError } = await supabase.from("groups").insert({
+
+        });
+
+
+    }
+
+
+
 }
 
 export { handleMessage }
